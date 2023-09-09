24fe1fa1427b70fe9c92bf22f4333397dabb5c33
gmth6789 <143313748+gmth6789@users.noreply.github.com>
2023-09-09 21:22:39 +0700

    1 file modified, 4 added

---
+++ components/Thenav.vue
@@ -0,0 +1,15 @@
+
+
+
+
+
+<template>
+<div Header class="diygw-title diygw-top pink">
+					<div Header class="flex flex-nowrap diygw-col-24 items-start" @click="navigateTo" data-type="page" data-url="index.html">
+						<div class="diygw-title diygw-top pink">
+							<i onclick="history.back(-1)" class="title-bar-icon diy-icon-back"></i> <span class="back-text" onclick="history.back(-1)">กลับ</span> <i onclick="location.href='index.html'" class="title-bar-icon diy-icon-home margin-left"></i>
+							<div class="title flex1 text-center textindent50">หน้าหลัก</div>
+						</div>
+					</div>
+				</div>
+  </template>
